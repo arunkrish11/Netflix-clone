2fe5749f2ba68f9cb88afbd64a1a99841ed16cb7
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-07-02 19:44:43 +0530

    row poster

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
 import Banner from './Components/Banner/Banner'
+import RowPoster from './Components/RowPoster/RowPoster'
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
+      <RowPoster/>
     </div>
   )
 }
