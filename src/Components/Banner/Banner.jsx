6bd818b9bfb46662b252bb6ebd34c33320e32a48
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-07-04 02:06:07 +0530

    dynamic bg image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,22 +1,39 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { API_KEY,imgeUrl } from '../../constansts/constants'
+// importing axios.js
+import axios from '../../axios'
 import './Banner.css'
 
-export class Banner extends Component {
-  render() {
-    return (
-      <div className='banner'>
+function Banner() {
+  const [movie, setMovie] = useState()
+
+  // We need movies detailes while loading or mounting Banner. So we use useEffect here.
+  // useEffect: This hook runs after every render cycle. Which means useEffect work while loading this Banner and execute code inside it. 
+  useEffect(() => {
+    // You can access API_KEY in bleow, for that you need to use in axion.get(`${API_KEY`}`)
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US` ).then((response) => {
+      // Movie detailes are in response.data.results[0]
+      console.log(response.data.results[3])
+      setMovie(response.data.results[3])
+    })
+  },[])
+  
+  return (
+    <div
+     style={{backgroundImage: `url(${movie ? imgeUrl+movie?.backdrop_path : "Loading" })`}}
+     className='banner'>
         <div className="content">
-            <h1 className="title">Movie Name</h1>
+            {/* if movie there show movie title or loading */}
+            <h1 className="title">{movie ? movie.title : 'Loading...'}</h1>
             <div className="banner_buttons">
               <button className="button">Play</button>
               <button className="button">My List</button>
             </div>
-            <h1 className="discription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            <h1 className="discription">{movie ? movie.overview : 'Loading...'}</h1>
         </div>
         <div className="fade_bottom"></div>
       </div>
     )
-  }
 }
 
 export default Banner
---
+++ src/constansts/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const imgeUrl = "https://image.tmdb.org/t/p/original"
+export const API_KEY = "29970835287777cd71d5f882c8c6a131";
