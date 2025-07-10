1a5f723604cb98d4673ba9b7814dba7310612392
arunkrish11 <tech.krish.arun.11@gmail.com>
2025-07-11 02:09:48 +0530

    improved with try catch method

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,39 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { API_KEY,imageUrl } from '../../Constants/constants'
+import React, { useEffect, useState } from "react";
+import { imageUrl } from "../../Constants/constants";
+
 // importing axios.js
-import axios from '../../axios'
-import './Banner.css'
+import "./Banner.css";
+import instance from "../../axios";
 
 function Banner() {
-  const [movie, setMovie] = useState()
-
-  // We need movies detailes while loading or mounting Banner. So we use useEffect here.
-  // useEffect: This hook runs after every render cycle. Which means useEffect work while loading this Banner and execute code inside it. 
-  useEffect(() => {
-    // You can access API_KEY in bleow, for that you need to use in axion.get(`${API_KEY`}`)
-    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US` ).then((response) => {
-      // Movie detailes are in response.data.results[0]
-      setMovie(response.data.results[0])
-      console.log(response.data.results[0]);
-    })
-  },[])
+  const [movie, setMovie] = useState();
   
+  // We need movies details while loading or mounting Banner. So we use useEffect here.
+  // useEffect: This hook runs after every render cycle. Which means useEffect work while loading this Banner and execute code inside it.
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    
+    const fetchMovie = async () => {
+      try {
+        const response = await instance.get(
+          `trending/all/week?api_key=${API_KEY}&language=en-US`
+        );
+        setMovie(response.data.results[0]);
+      } catch (error) {
+        console.log(API_KEY);
+        console.error("Banner movie not found", error.message);
+      }
+    };
+
+    fetchMovie();
+  }, []);
+
   return (
     <div
-     style={{backgroundImage: `url(${movie ? imageUrl+movie?.backdrop_path : "Loading" })`}}
-     className='banner'>
-        <div className="content">
-            {/* if movie there show movie title or loading */}
-            <h1 className="title">{movie ? movie.title : 'Loading...'}</h1>
-            <div className="banner_buttons">
-              <button className="button">Play</button>
-              <button className="button">My List</button>
-            </div>
-            <h1 className="discription">{movie ? movie.overview : 'Loading...'}</h1>
+      style={{
+        backgroundImage: `url(${
+          movie ? imageUrl + movie?.backdrop_path : "Loading"
+        })`,
+      }}
+      className="banner"
+    >
+      <div className="content">
+        {/* if movie there show movie title or loading */}
+        <h1 className="title">{movie ? movie.title : "Loading..."}</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
         </div>
-        <div className="fade_bottom"></div>
+        <h1 className="description">{movie ? movie.overview : "Loading..."}</h1>
       </div>
-    )
+      <div className="fade_bottom"></div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
